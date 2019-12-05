14a62e4f33707f770f7b7e56d4b372668cd6274d
Sirb Daniel <daniel.sirb@accesa.eu>
2019-12-05 15:19:26 +0200

    Just for example

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Residence.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,35 @@ namespace ResidenceShoppe
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private string _text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+            }
+        }
+
+        public StudioViewModel StudioVM { get; }
+
         public MainWindow()
         {
             InitializeComponent();
+            StudioVM = new StudioViewModel();
+            DataContext = StudioVM;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Text += DateTime.Now.ToShortDateString();
+            StudioVM.FloorNumber += 1;
         }
     }
 }
